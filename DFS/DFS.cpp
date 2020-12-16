5e32724a4b39e4926b2c1c6895315670332c0387
fizzfuzzhk <fizzfuzzhk@gmail.com>
2020-12-16 22:11:05 +0900

    commit

--- DFS/DFS.cpp
+++ DFS/DFS.cpp
@@ -11,6 +11,7 @@ void dfs(const Graph &G, int v){
     seen[v] = true; //vを訪問済みにする
 
     //vからいける頂点 next_vについて
+    //例えばvが0の場合、0から訪問できる各頂点について、最も深く到達できる終点を調べていく
     for (auto next_v : G[v]){
         if(seen[next_v]) continue; //next_vが訪問済みだったらスルー
         dfs(G, next_v); //再帰的に探索
